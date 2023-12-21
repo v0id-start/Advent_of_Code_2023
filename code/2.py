87776c9dfd387caa5fc249623c8c1902c34dc500
ehscs-dev <73728431+ehscs-dev@users.noreply.github.com>
2023-12-21 04:34:36 -0800

    progress?

--- code/2.py
+++ code/2.py
@@ -1,5 +1,33 @@
 cubes = {"red": 0, "green": 0, "blue": 0}
 
+with open("data/2.txt") as f:
+    lines = f.readlines()
+    power_sum = 0
+    for line in lines:
+        line = line.strip().split(": ")[1]
+        game = line.split(";")
+        
+        for bag_set in game:
+            colors = bag_set.split(", ")
+            for color in colors:
+                color = color.lstrip()
+                nums = color.split(" ")
+                s_color = nums[1]
+                num_cubes = int(nums[0])
+                print(nums, s_color)
+                if num_cubes > cubes[s_color]:
+                    cubes[s_color] = num_cubes
+            print(cubes)
+            power = cubes["red"] * cubes["green"] * cubes["blue"]
+            print(power)
+            power_sum += power
+            for c in cubes.keys():
+                cubes[c] = 0
+
+    print(power_sum)
+
+"""
+cubes = {"red": 0, "green": 0, "blue": 0}
 
 def set_possible():
     return cubes["red"] <= 12 and cubes["green"] <= 13 and cubes["blue"] <= 14
@@ -29,3 +57,4 @@ with open("data/2.txt") as f:
 
         cur_game += 1
     print(id_sum)
+"""
